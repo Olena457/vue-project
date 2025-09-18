1425276b749f730db47fde77b1a67f20f1cc0464
Olena457 <olenademianenko42@gmail.com>
2025-09-18 22:10:35 +0300

    Update GreetingView.vue

--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -5,7 +5,7 @@ import BaseLayout from '@/BaseLayout/BaseLayout.vue'
 <template>
   <BaseLayout>
     <div class="text-center">
-      <img class="inline mb-6" src="../assets/image/map-pin.svg" alt="" />
+      <img class="inline mb-6" src="../assets/image/map-pin.svg" alt="pin" />
       <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
       <p class="leading-6 mb-11">
         Простий, зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, також ті, в
