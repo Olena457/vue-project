b9a4860290b74c2114cf111287cfb100ab6ca42a
Olena457 <olenademianenko42@gmail.com>
2025-09-20 11:35:29 +0300

    Create RegistrationForm.vue

---
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive, toRaw } from 'vue'
+import IInput from '@/components/IInput/IInput.vue'
+import IButton from '@/components/IButton/IButton.vue'
+
+const props = defineProps({
+  isLoading: {
+    default: false,
+    type: Boolean,
+  },
+})
+const emit = defineEmits(['submit'])
+const userData = reactive({
+  name: '',
+  email: '',
+  password: '',
+})
+</script>
+
+<template>
+  <form @submit.prevent="emit('submsit', toRaw(userData))">
+    <IInput class="mb-4" label="Full name" v-model="userData.name" />
+    <IInput class="mb-4" label="Email" placeholder="email@gmail.com" v-model="userData.email" />
+    <IInput class="mb-4" label="Password" type="password" v-model="userData.name" />
+    <IButton class="mt-10 w-full" variant="gradient" type="submit" :is-loading="props.isLoading">
+      Create account
+    </IButton>
+  </form>
+</template>
