0a6249691cbb5a8a8c62591c2ad35435abd897a5
Olena457 <olenademianenko42@gmail.com>
2025-09-20 11:05:34 +0300

    created autheview and corect base css

---
+++ src/components/map/settings.js
@@ -0,0 +1,5 @@
+// import {token}from './token
+// export const mapSettings = {
+// style: 'mapbox://styles/mapbox/streets-v12',
+// apiToken: token
+// }
---
+++ src/views/AuthView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+import BaseLayout from '@/Layout/BaseLayout.vue'
+</script>
+<template>
+  <BaseLayout>
+    <section class="w-full bg-white p-10 rounded-2xl">
+      <div class="flex justify-center gap-10 mb-10">
+        <router-link
+          to="registration"
+          class="text-gray"
+          exact-active-class="text-[var(--text-primary)]"
+        >
+          Create account
+        </router-link>
+        <router-link to="login" class="text-gray" exact-active-class="text-[var(--text-primary)]">
+          Sing in
+        </router-link>
+      </div>
+      <router-view></router-view>
+    </section>
+  </BaseLayout>
+</template>
