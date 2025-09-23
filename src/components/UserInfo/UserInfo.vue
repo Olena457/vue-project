14064b1aea57ec57406fefa589d1aac931c4bbd3
Olena457 <olenademianenko42@gmail.com>
2025-09-23 16:52:24 +0300

    created user info

---
+++ src/components/UserInfo/UserInfo.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useMutation } from '@/composables/useMutation.js'
+import { getUserInfo } from '@/api/user/index.js'
+
+import UserIcon from './UserIcon.vue'
+const {
+  data: userInfo,
+  mutation: getUser,
+  isLoading,
+} = useMutation({
+  mutationFn: () => getUserInfo(),
+})
+onMounted(() => {
+  getUser()
+})
+</script>
+<template>
+  <div
+    class="flex sticky top-0 items-center gap-3 text-black bg-[#ffe6dc] border-b-2 rounded-bl-2xl rounded-br-2xl px-6 py-4 mb-10"
+  >
+    <div class="w-10 h-10 flex item-center justify-center rounded-full color-primary bg-primary">
+      <UserIcon class="text-white" />
+    </div>
+    <span v-if="isLoading">Loading...</span>
+    <span v-if="userInfo">{{ userInfo.data.name }}</span>
+  </div>
+</template>
