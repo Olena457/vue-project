c9b9f6201f5f6876b48c892bb554c92a582e766d
Olena457 <olenademianenko42@gmail.com>
2025-09-24 23:20:29 +0300

    corected main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,3 @@
-// import './assets/main.css'
-
-// import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-// import { router } from './router/index.js'
-// import { useAuthService, TOKEN_KEY } from './api/authService'
-
-// import App from './App.vue'
-// const token = localStorage.getItem(TOKEN_KEY)
-// if (token) {
-//   useAuthService.setToken(token)
-// }
-// const app = createApp(App)
-
-// app.use(createPinia())
-// app.use(router)
-// app.mount('#app')
 import './assets/main.css'
 
 import { createApp } from 'vue'
