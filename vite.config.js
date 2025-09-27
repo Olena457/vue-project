5aba116c5d9f97c4c41421efb132c794ccb8ed41
Olena457 <olenademianenko42@gmail.com>
2025-09-27 11:18:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const packageName = id.split('node_modules/')[1].split('/')[0]
+
+            if (packageName.includes('vue')) {
+              return 'vendor_vue'
+            }
+
+            return `vendor_${packageName}`
+          }
+        },
+
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: (chunkInfo) => {
+          const facades = chunkInfo.facades || []
+          if (facades.some((f) => f.includes('views'))) {
+            return 'views/[name]-[hash].js'
+          }
+          return 'chunks/[name]-[hash].js'
+        },
+      },
     },
   },
 })
