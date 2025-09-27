5316a53b25c83ce5a24cba2197e291dfe17d3e6c
Olena457 <olenademianenko42@gmail.com>
2025-09-27 13:41:21 +0300

    Update settings.js

--- src/map/settings.js
+++ src/map/settings.js
@@ -1,5 +1,5 @@
-import { token } from './token.js'
+// src/map/settings.js
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
-  apiToken: token,
+  apiToken: import.meta.env.VITE_MAP_TOKEN,
 }
